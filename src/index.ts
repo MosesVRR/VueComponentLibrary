93d86ef3553e42c82e2a1b83a8ec3f89d4e40a39
mosesvrr <mosesvrr@gmail.com>
2025-02-28 22:25:41 -0500

    import path module for index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from './components/Button.vue'
+export { default as HelloWorld } from './components/HelloWorld.vue'
+// Add more exports as needed
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueComponentLibrary',
+      fileName: (format) => `vue-component-library.${format}.js`
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
