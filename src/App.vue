6eb6750998977c80ff396a3d01a712e253adf1aa
mosesvrr <mosesvrr@gmail.com>
2025-02-28 22:15:54 -0500

    add button component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
+
+const handleButtonClick = () => {
+  alert('Button clicked!')
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Button label="Click Me" @click="handleButtonClick" />
 </template>
 
 <style scoped>
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="handleClick">{{ label }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps<{ label: string }>()
+const emit = defineEmits<{ (e: 'click'): void }>()
+
+const handleClick = () => {
+  emit('click')
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  background-color: #42b883;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #38a169;
+}
+</style>
